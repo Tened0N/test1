6693c8da6ef7df65c67ea074539ebd568a820da3
Tened0N <valentin.gheorghiu@outlook.com>
2025-06-05 19:41:53 +0000

    added subnet

--- V/main.tf
+++ V/main.tf
@@ -1,4 +1,20 @@
-resource "azurerm_resource_group" "Orgrimmar" {
-  name     = "Thrall"
-  location = "West Europe"
+resource "azurerm_resource_group" "Thrall" {
+   location = "West Europe"
+   name = "Thrall"
+    tags = { owner = "valentin.gheorghiu@redbull.com"}
+}
+# Virtual Network
+resource "azurerm_virtual_network" "Cip_Net" {
+  name                = "Cip_Net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.Thrall.name
+  resource_group_name = azurerm_resource_group.Thrall.name
+}
+ 
+# Subnet 1
+resource "azurerm_subnet" "Orgrimmar" {
+  name                 = "Orgrimmar"
+  resource_group_name  = azurerm_resource_group.Thrall.name
+  virtual_network_name = azurerm_virtual_network.Thrall.name
+  address_prefixes     = ["10.0.6.0/24"]
 }
